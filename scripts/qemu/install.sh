28ec5aaaf8fbcb7666eaf9582d22f0c2f31b15d8
DaeRyong Jeong <threeearcat@gmail.com>
2019-06-04 03:04:46 +0000

    minor fix

--- scripts/qemu/install.sh
+++ scripts/qemu/install.sh
@@ -11,9 +11,9 @@ pushd build
 # This is just placehold. Actual HYPEADDR will be filled when executing exp.sh
 HYPEADDR='0x0'
 
-../configure --target-list=x86_64-softmmu --cc=$GCC7_HOME/install/bin/gcc --cxx=$GCC7_HOME/install/bin/g++
+../configure --target-list=x86_64-softmmu --cc=$GCC_HOME/install/bin/gcc --cxx=$GCC_HOME/install/bin/g++
 
-CC=$GCC7_HOME/install/bin/gcc CFLAGS="-D_HYPERCALL_ADDR=$HYPEADDR" make -j`nproc`
+CC=$GCC_HOME/install/bin/gcc CFLAGS="-D_HYPERCALL_ADDR=$HYPEADDR" make -j`nproc`
 
 popd
 popd>/dev/null
